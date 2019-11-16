2c2d66226795efbbdf8c2b514e31864dd836c301
Da Chen <da6@ksu.edu>
2019-11-16 17:39:12 -0600

    homework8.3

--- Website/Pages/Menu.cshtml.cs
+++ Website/Pages/Menu.cshtml.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using DinoDiner.Menu;
 
 namespace Website.Pages
 {
     public class MenuModel : PageModel
     {
+        public Menu Menu { get; } = new Menu();
         public void OnGet()
         {
 
