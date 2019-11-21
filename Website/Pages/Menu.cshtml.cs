208bf777595f3f47182e893e74b97c8ad0421080
Da Chen <da6@ksu.edu>
2019-11-21 17:07:12 -0600

    homework10.2

--- Website/Pages/Menu.cshtml.cs
+++ Website/Pages/Menu.cshtml.cs
@@ -10,16 +10,54 @@ namespace Website.Pages
 {
     public class MenuModel : PageModel
     {
+
+        [BindProperty]
+        public string search { get; set; }
+
+        [BindProperty]
+        public List<string> type { get; set; } = new List<string>();
+
         public Menu Menu { get; } = new Menu();
+
+        public List<IMenuItem> AvailableMenuItems;
         public void OnGet()
         {
-
+            AvailableMenuItems = Menu.AvailableMenuItems;
         }
 
 
         public void Onpost()
         {
-         
+            AvailableMenuItems = Menu.AvailableMenuItems;
+
+            if (search != null && mapp.Count != 0)
+            {
+                Movies = MovieDatabase.SearchAndFilter(search, mapp);
+            }
+            else if (mapp.Count != 0)
+            {
+                Movies = MovieDatabase.Filter(mapp);
+            }
+
+            else if (search != null)
+            {
+                Movies = MovieDatabase.Search(Movies, search);
+            }
+
+
+            if (mapp.Count != 0)
+            {
+                Movies = MovieDatabase.FilterByMPAA(Movies, mapp);
+            }
+
+            if (minIMDB != null)
+            {
+                Movies = MovieDatabase.FilterByBinIMDB(Movies, (float)minIMDB);
+            }
+            if (maxIMDB != null)
+            {
+                Movies = MovieDatabase.FilterByMaxIMDB(Movies, (float)maxIMDB);
+            }
         }
     }
 }
